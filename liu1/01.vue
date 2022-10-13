a2dee4e872efc7bfbd119a771a64007be418f5b1
liusir-hub <994866751@qq.com>
2022-10-13 10:38:14 +0800

    2022-10-13

---
+++ liu1/01.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script>
+export default {
+  data () {
+    return {
+      timer: null
+    }
+  },
+  mouted () {
+    this.timer = setInterval(() => { }, 1000)
+    this.$once('hook:beforeDestory', () => { clearInterval(this.timer) })
+  }
+}
+</script>
+
+<style>
+</style>
